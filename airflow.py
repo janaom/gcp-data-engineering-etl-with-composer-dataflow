b17569966e03035aa147344386559261fdf86b2b
janaom <83917694+janaom@users.noreply.github.com>
2023-12-07 19:48:26 +0100

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -2,25 +2,30 @@ from airflow import models
 from datetime import datetime, timedelta
 from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator
 
+#Default arguments for the DAG
 default_args = {
     'owner': 'Airflow',
     'start_date': datetime(2023, 12, 3),
     'retries': 0,
     'retry_delay': timedelta(seconds=50),
 	'dataflow_default_options': {
-        'project': 'food-orders-407014',
-        'region': 'us-central1', #composer region
+        'project': 'project-id',
+        'region': 'composer-region', 
 		'runner': 'DataflowRunner'
     }
 }
 
+#Define the DAG
 with models.DAG('food_orders_dag',
          default_args=default_args,
          schedule_interval='@daily',
          catchup=False) as dag:
     
+    #DataFlowPythonOperator to execute the Beam pipeline
     t1 = DataFlowPythonOperator(
         task_id='beamtask',
-        py_file='gs://us-central1-food-orders-dev-752d1f51-bucket/beam.py',
-        options={'input' : 'gs://food-orders-us/food_daily.csv'}
+        #Path to the Beam pipeline file
+        py_file='gs://composer-bucket/beam.py',
+        #Input file for the pipeline
+        options={'input' : 'gs://your-bucket/food_daily.csv'}
     )
